87782baf707258870a548544c53318728f402970
aidact3 <aidacarabot.tosar@gmail.com>
2025-04-03 22:59:59 +0200

    Design Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Award, BadgeDollarSign, ChartColumn } from 'lucide-react';
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Overview</Link></li>
-        <li><Link to="/transactions">Transactions</Link></li>
-        <li><Link to="/goals">Goals</Link></li>
+        <li><Link to="/"><ChartColumn className="nav-icon" />Overview</Link></li>
+        <li><Link to="/transactions"><BadgeDollarSign className="nav-icon" />Transactions</Link></li>
+        <li><Link to="/goals"> <Award className="nav-icon" />Goals</Link></li>
       </ul>
     </nav>
   )
